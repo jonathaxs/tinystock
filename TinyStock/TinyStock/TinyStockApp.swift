d5fcf282e8c98020595275c6801b239bd8af6c9f
Jonathas Motta <jonathasmrt@gmail.com>
2026-07-30 22:02:02 -0300

    chore(xcode): setup target settings

---
+++ TinyStock/TinyStock/TinyStockApp.swift
@@ -0,0 +1,32 @@
+//
+//  TinyStockApp.swift
+//  TinyStock
+//
+//  Created by Jonathas Motta on 20/07/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TinyStockApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TinyStock/TinyStockTests/TinyStockTests.swift
@@ -0,0 +1,19 @@
+//
+//  TinyStockTests.swift
+//  TinyStockTests
+//
+//  Created by Jonathas Motta on 20/07/26.
+//
+
+import Testing
+@testable import TinyStock
+
+struct TinyStockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
