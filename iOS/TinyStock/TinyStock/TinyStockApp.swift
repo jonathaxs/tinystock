1c66a9565b7e736d978e77ce6009b21e5b80e8c6
Jonathas Motta <jonathasmrt@gmail.com>
2026-08-30 22:56:35 -0300

    chore(ios): adicionado modelo de movimentacao ao esquema

--- iOS/TinyStock/TinyStock/TinyStockApp.swift
+++ iOS/TinyStock/TinyStock/TinyStockApp.swift
@@ -24,6 +24,7 @@ struct TinyStockApp: App {
             StoreProfile.self,
             Product.self,
             ProductVariant.self,
+            StockMovement.self,
             Sale.self,
             SaleItem.self
         ])
